32da2d69a6164697b76448912808ca7217f1027f
A Yong <andraceyong@gmail.com>
2017-06-05 17:17:11 -0400

    commit

--- main.go
+++ main.go
@@ -1,83 +1,51 @@
 package main
 
-
-import (
-	_ "github.com/go-sql-driver/mysql"
-	"database/sql"
+import("net/http"
 	"fmt"
+	"html/template"
 )
 
-func main() {
-
-	db, err := sql.Open("mysql", "ayong:zzz@/test?charset=utf8")
-	checkErr(err)
-
-	//------------------------------------------------------------------------------
-	// insert
-	fmt.Println("aa1")
-	stmt, err := db.Prepare("INSERT userinfo SET username=?,departname=?,created=?")
-	res, err := stmt.Exec("HELLO_abc", "MARKETING", "2012-12-09")
-	if res==nil {fmt.Println("nil!!!!")}
-	//id, err := res.LastInsertId()
-	//fmt.Println(id)
-	fmt.Println("aa2")
-
-	//checkErr(err)
-	/*
-	// update
-	stmt, err = db.Prepare("update userinfo set username=? where uid=?")
-	checkErr(err)
-
-	res, err = stmt.Exec("NATEYONG", id)
-	checkErr(err)
-
-	affect, err := res.RowsAffected()
-	checkErr(err)
-
-	fmt.Println(affect)
-	*/
-
-	/*
-	// query
-	rows, err := db.Query("SELECT * FROM userinfo")
-	checkErr(err)
-
-	for rows.Next() {
-		var uid int
-		var username string
-		var department string
-		var created string
-		err = rows.Scan(&uid, &username, &department, &created)
-		checkErr(err)
-		fmt.Println(uid)
-		fmt.Println(username)
-		fmt.Println(department)
-		fmt.Println(created)
-	}
-*/
-
-	/*
-	// delete
-	stmt, err = db.Prepare("delete from userinfo where uid=?")
-	checkErr(err)
-
-	res, err = stmt.Exec(id)
-	checkErr(err)
-
-	affect, err = res.RowsAffected()
-	checkErr(err)
-
-	fmt.Println(affect)
-	*/
-
-	db.Close()
-
+func dummy() (_, err error) {
+	return
 }
 
-func checkErr(err error) {
-	//fmt.Println("Ok. no error found")
-	if err != nil {
-		fmt.Println("Found an error!")
-		panic(err)
+func index(writer http.ResponseWriter, r *http.Request) {
+	tmpl_file1 := "test1.html"
+
+
+	/*
+	tmpl_filesMany :=[]string  {
+		"test1.html",
+		"test2.html",
 	}
+	*/
+
+
+
+
+	templates :=template.Must(template.ParseFiles(tmpl_file1))
+	//templates2 :=template.Must(template.ParseFiles(tmpl_filesMany...))
+
+	dummy1, _:= dummy()
+	templates.ExecuteTemplate(writer, "layout", dummy1)
+	//templates2.ExecuteTemplate(w, "layout", dummy1)
+
+
+}
+func main() {
+	fmt.Println("starting server + mux")
+
+	mux :=http.NewServeMux()
+
+	files := http.FileServer(http.Dir("public"))
+	mux.Handle("/static/", http.StripPrefix("/static/", files))
+
+	mux.HandleFunc("/", index)
+	server :=&http.Server{
+		Addr: "0.0.0.0:7002",
+		Handler : mux,
+
+	}
+	fmt.Println("listening...")
+	server.ListenAndServe()
 }
