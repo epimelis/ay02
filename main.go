abe344d6dc8313ea17c12bdc85de4199b7fbb359
A Yong <andraceyong@gmail.com>
2017-06-05 10:40:51 -0400

    commit

--- main.go
+++ main.go
@@ -1,53 +1,83 @@
 package main
 
-import("net/http"
+
+import (
+	_ "github.com/go-sql-driver/mysql"
+	"database/sql"
 	"fmt"
-	"html/template"
 )
 
-type Thread struct {
-	Id        int
-	Uuid      string
-	Topic     string
-	UserId    int
-}
-
-func Threads() (threads []Thread, err error) {
-
-	return
-}
-
-
-func dummy() (_, err error) {
-	return
-}
-
-func index(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("aa1")
-	tmpl_file1 := "test.html"
-	/*tmpl_files :=[]string  {
-		"test.html",
-	}*/
-
-	fmt.Println("aa2")
-	//templates :=template.Must(template.ParseFiles(tmpl_files...))
-	templates :=template.Must(template.ParseFiles(tmpl_file1))
-	fmt.Println("aa3")
-	dummy1, _:= dummy()
-
-	templates.ExecuteTemplate(w, "layout", dummy1)
-
-
-}
 func main() {
-	fmt.Println("starting server")
-	mux :=http.NewServeMux()
-	mux.HandleFunc("/", index)
-	server :=&http.Server{
-		Addr: "0.0.0.0:8080",
-		Handler : mux,
 
+	db, err := sql.Open("mysql", "ayong:zzz@/test?charset=utf8")
+	checkErr(err)
+
+	//------------------------------------------------------------------------------
+	// insert
+	fmt.Println("aa1")
+	stmt, err := db.Prepare("INSERT userinfo SET username=?,departname=?,created=?")
+	res, err := stmt.Exec("HELLO_abc", "MARKETING", "2012-12-09")
+	if res==nil {fmt.Println("nil!!!!")}
+	//id, err := res.LastInsertId()
+	//fmt.Println(id)
+	fmt.Println("aa2")
+
+	//checkErr(err)
+	/*
+	// update
+	stmt, err = db.Prepare("update userinfo set username=? where uid=?")
+	checkErr(err)
+
+	res, err = stmt.Exec("NATEYONG", id)
+	checkErr(err)
+
+	affect, err := res.RowsAffected()
+	checkErr(err)
+
+	fmt.Println(affect)
+	*/
+
+	/*
+	// query
+	rows, err := db.Query("SELECT * FROM userinfo")
+	checkErr(err)
+
+	for rows.Next() {
+		var uid int
+		var username string
+		var department string
+		var created string
+		err = rows.Scan(&uid, &username, &department, &created)
+		checkErr(err)
+		fmt.Println(uid)
+		fmt.Println(username)
+		fmt.Println(department)
+		fmt.Println(created)
 	}
-	fmt.Println("listening on port 8080...")
-	server.ListenAndServe()
+*/
+
+	/*
+	// delete
+	stmt, err = db.Prepare("delete from userinfo where uid=?")
+	checkErr(err)
+
+	res, err = stmt.Exec(id)
+	checkErr(err)
+
+	affect, err = res.RowsAffected()
+	checkErr(err)
+
+	fmt.Println(affect)
+	*/
+
+	db.Close()
+
+}
+
+func checkErr(err error) {
+	//fmt.Println("Ok. no error found")
+	if err != nil {
+		fmt.Println("Found an error!")
+		panic(err)
+	}
 }
